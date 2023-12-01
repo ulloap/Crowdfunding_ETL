cff87c59eeb5d2ca38f815e71e68c5882da0ec14
ulloap <ulloanancy@yahoo.com>
2023-11-30 23:40:11 -0600

    official_debugged

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,3 +1,35 @@
+CREATE TABLE "Contacts" (
+    "contact_id" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+	"email" varchar   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+DROP TABLE "Contacts"
+
+CREATE TABLE "Category" (
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+DROP TABLE "Category"
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+DROP TABLE "Subcategory"
+
 CREATE TABLE "Campaign" (
     "cf_id" varchar   NOT NULL,
     "contact_id" varchar   NOT NULL,
@@ -18,35 +50,7 @@ CREATE TABLE "Campaign" (
      )
 );
 
-DROP TABLE "Campaign";
-
-CREATE TABLE "Contacts" (
-    "email" varchar   NOT NULL,
-    "contact_id" varchar   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
-);
-
-DROP TABLE "Contacts";
-
-CREATE TABLE "Category" (
-    "category_id" varchar   NOT NULL,
-    "category" varchar   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
-        "category_id"
-     )
-);
-
-CREATE TABLE "Subcategory" (
-    "subcategory_id" varchar   NOT NULL,
-    "subcategory" varchar   NOT NULL,
-    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
-);
+DROP TABLE "Campaign"
 
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "Contacts" ("contact_id");
--- Resources/erd_crowdfunding_db.sql
+++ Resources/erd_crowdfunding_db.sql
@@ -2,51 +2,59 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "company_name" varchar()   NOT NULL,
-    "description" varchar()   NOT NULL,
-    "goal" int   NOT NULL,
-    "pledged" int   NOT NULL,
-    "outcome" varchar()   NOT NULL,
-    "backers_count" int   NOT NULL,
-    "country" varchar()   NOT NULL,
-    "currency" varchar()   NOT NULL,
-    "launched_date" varchar()   NOT NULL,
-    "end_date" varchar()   NOT NULL,
-    "category_id" varchar()   NOT NULL,
-    "subcategory_id" varchar()   NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
-
 CREATE TABLE "Contacts" (
-    "email" varchar()   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "first_name" varchar()   NOT NULL,
-    "last_name" varchar()   NOT NULL,
+    "contact_id" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+	"email" varchar   NOT NULL,
     CONSTRAINT "pk_Contacts" PRIMARY KEY (
         "contact_id"
      )
 );
 
+DROP TABLE "Contacts"
+
 CREATE TABLE "Category" (
-    "category_id" varchar()   NOT NULL,
-    "category" varchar()   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY (
         "category_id"
      )
 );
 
+DROP TABLE "Category"
+
 CREATE TABLE "Subcategory" (
-    "subcategory_id" varchar()   NOT NULL,
-    "subcategory" varchar()   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
         "subcategory_id"
      )
 );
+
+DROP TABLE "Subcategory"
+
+CREATE TABLE "Campaign" (
+    "cf_id" varchar   NOT NULL,
+    "contact_id" varchar   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
+    "goal" varchar   NOT NULL,
+    "pledged" varchar   NOT NULL,
+    "outcome" varchar   NOT NULL,
+    "backers_count" varchar   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
+    "launched_date" varchar   NOT NULL,
+    "end_date" varchar   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+DROP TABLE "Campaign"
 
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "Contacts" ("contact_id");
@@ -56,4 +64,3 @@ REFERENCES "Category" ("category_id");
 
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "Subcategory" ("subcategory_id");
-
